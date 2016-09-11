17c7b579adee56807e1032e1d740ce269986ce33
Michael Ambass <michael.ambass@gmail.com>
2016-09-11 17:45:19 +0200

    Data-binding with object

--- app/app.component.js
+++ app/app.component.js
@@ -12,16 +12,25 @@ var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        this.hero = 'Windstorm';
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>{{title}}</h1><h2>{{hero}}</h2>'
+            template: "\n      <h1>{{title}}</h1>\n      <h2>{{hero.name}} details!</h2>\n      <div><label>id: </label>{{hero.id}}</div>\n      <div><label>name: </label>{{hero.name}}</div>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var Herro = (function () {
+    function Herro() {
+    }
+    return Herro;
+}());
+exports.Herro = Herro;
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}}</h2>'
+    template:`
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div><label>name: </label>{{hero.name}}</div>
+    `
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
+export class Herro {
+  id: number;
+  name: string;
 }
