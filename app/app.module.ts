b58619e41780a6f0091735c08e2a326387985b08
Michael Ambass <michael.ambass@gmail.com>
2016-09-11 17:50:00 +0200

    Two-binding

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; // for two-way binding
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
